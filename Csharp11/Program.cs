e0ed023101219d2907ffdb280d96cee0ae3aed0d
Guven Kurt <guven.kurt@checkout.com>
2022-04-01 17:08:09 +0200

    Add project files.

---
+++ Csharp11/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+//Newlines in interpolated strings
+var str = $"{Subscriber
+    .GetSubscribers("name")
+    .FirstOrDefault()}";
+
+Console.WriteLine(str);
+
+// List Patterns
+var list = new[] { "1", "2", "3", "4", "5", "6" };
+
+Console.WriteLine(list is ["1", .., "6"]);
+
+if (list is ["1", .. var rest, "6"])
+{
+    foreach (var item in rest)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
+
+// Bang Bang
+try
+{
+    Subscriber.GetSubscribers(null);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.ReadKey();
+
+class Subscriber
+{
+    public static List<string> GetSubscribers(string userName)
+    {
+        if (userName is null)
+        {
+            throw new ArgumentNullException(nameof(userName));
+        }
+        return new List<string>();
+    }
+}
